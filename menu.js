762ee2d4a3af57b4016d9d96fd0b5063469e9a11
Nathalie Pino Paz <105008072+nathaliepp@users.noreply.github.com>
2022-07-11 22:11:28 -0600

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,17 @@
+let btnMenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+
+btnMenu.addEventListener('click', function(){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
+
+
+$(window).load(function() {
+    jQuery('#all').click();
+    return false;
+});
+
+
+
